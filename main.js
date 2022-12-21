0ff2cb88417606fbac030d82bd76a7ed1634d3ca
SergioLU18 <slopezurzaiz@gmail.com>
2022-12-20 20:34:00 -0600

    feat: cleaned code and added comments for time complexity

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 // Import libraries for making requests
 const https = require('https');
 
-
-
 // Create map for storing activities
 var storedActivities = new Map();
 
@@ -26,6 +24,7 @@ const postOptions = {
 }
 
 // Sort all activities by first_seen_at
+// Since we are using JavaScript's sort function, the time complexity is O(n log n)
 function sortByFirstSeenAt(activities) {
     activities.sort((a, b) => {
         if(a.first_seen_at < b.first_seen_at) { 
@@ -38,11 +37,12 @@ function sortByFirstSeenAt(activities) {
     });
 }
 
+// Post all activities to endpoint
+// This is an async function, so the time complexity is O(n)
 function postActivities(activitiesJSON) {
     return new Promise(resolve => {
         const req = https.request("https://api.slangapp.com/challenges/v1/activities/sessions", postOptions, (res) => {
-            console.log('statusCode:', res.statusCode);
-            console.log('headers:', res.headers);
+            console.log('Post status Code:', res.statusCode);
 
             res.on('data', (d) => {
                 console.log(d);
@@ -59,6 +59,7 @@ function postActivities(activitiesJSON) {
 }
 
 // Get all activities from API
+// This is an async function, so the time complexity is O(n)
 function getActivities() {
     return new Promise(resolve => {
         https.get('https://api.slangapp.com/challenges/v1/activities', getOptions, (res) => {
@@ -78,6 +79,7 @@ function getActivities() {
 }
 
 // Create a new session object
+// We are only declaring variables here, so the time complexity is O(1)
 function createSessionObject(activity) {
     let newSession = {
         'ended_at': '',
@@ -97,6 +99,11 @@ function createSessionObject(activity) {
 }
 
 // Process all activities
+// This function has 2 cases: if activity is not in cache, and if activity is in cache. To check what case we are working for, we only need
+// to check if the activity is in the cache, so the time complexity is O(1) due to sets having constant time complexity for checking if an element is in the set.
+// If the activity is not in the cache, we are creating a new session object and adding it to the map. This is O(1) due to maps having constant time complexity for adding elements.
+// If the activity is in the cache, we are checking if the activity is in the same session. This is O(1) due to maps having constant time complexity for getting the last element in the map.
+// But, since we are in fact iterating over all activities, the time complexity is O(n) where n is the number of activities.
 function processActivities(activities) {
     for(const i of activities) {
         // If activity is not in cache, add it to cache and create new activity object in map
